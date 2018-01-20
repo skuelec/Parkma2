dada6d6fbfee8f3000af4f716eb4c7bb92516aaa
efI95 <efraim.guias@gmail.com>
2018-01-20 11:58:28 +0100

    parkingActivity

--- app/src/main/java/at/se/fhcampus/parkma2/ParkingActivity.java
+++ app/src/main/java/at/se/fhcampus/parkma2/ParkingActivity.java
@@ -1,14 +1,13 @@
 package at.se.fhcampus.parkma2;
 
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class ParkingActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parking);
     }
-
 }
