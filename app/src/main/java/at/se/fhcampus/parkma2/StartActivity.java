91edeeb7d9fb55513b0fd36340d4b6f4371384f6
“filipmil” <filip.milinkovic@stud.fh-campuswien.ac.at>
2018-01-20 11:48:35 +0100

    Animation Layout

--- app/src/main/java/at/se/fhcampus/parkma2/StartActivity.java
+++ app/src/main/java/at/se/fhcampus/parkma2/StartActivity.java
@@ -3,13 +3,28 @@ package at.se.fhcampus.parkma2;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.graphics.drawable.AnimationDrawable;
+import android.widget.RelativeLayout;
 
 public class StartActivity extends AppCompatActivity {
+
+    AnimationDrawable animationDrawable;
+    RelativeLayout relativeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+
+        relativeLayout = (RelativeLayout)findViewById(R.id.relativeLayout);
+// onCreate
+        animationDrawable =(AnimationDrawable)relativeLayout.getBackground();
+
+        animationDrawable.setEnterFadeDuration(5000);
+        animationDrawable.setExitFadeDuration(2000);
+// onResume
+        animationDrawable.start();
 
         try {
             Thread.sleep(3000);
