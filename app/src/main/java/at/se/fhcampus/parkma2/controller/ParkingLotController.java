91283ef3a1122a8447e1f9bb2d67efa9ca5669ff
Selman <kuelec.selman@hotmail.com>
2018-07-16 22:33:09 +0200

    Parkma4

---
+++ app/src/main/java/at/se/fhcampus/parkma2/controller/ParkingLotController.java
@@ -0,0 +1,42 @@
+package at.se.fhcampus.parkma2.controller;
+
+
+import java.util.ArrayList;
+
+import at.se.fhcampus.parkma2.models.ParkingLot;
+
+public class ParkingLotController {
+
+    private ArrayList<ParkingLot> parkingLots = new ArrayList<ParkingLot>();
+
+    public ParkingLotController(){
+        populateData();
+    }
+
+    public ArrayList<ParkingLot> getParkingLots() {
+        return parkingLots;
+    }
+
+
+    private void populateData(){
+        parkingLots.add(new ParkingLot("1","frei"));
+        parkingLots.add(new ParkingLot("2","frei"));
+        parkingLots.add(new ParkingLot("3","frei"));
+        parkingLots.add(new ParkingLot("4","frei"));
+        parkingLots.add(new ParkingLot("5","frei"));
+        parkingLots.add(new ParkingLot("6","frei"));
+        parkingLots.add(new ParkingLot("7","besetzt"));
+
+
+    }
+
+
+    public void setBesetzen(ParkingLot parkingLot){
+
+        if(parkingLot.getCheckBesetzt() == -1){
+
+
+        }
+
+    }
+}
